59866953348381b4c12145dbf5cad1e9aca1aa0f
maokebing <kebing1011@163.com>
2015-09-04 12:35:52 +0800

    Initial Commit

---
+++ SHA1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SHA1
+//
+//  Created by mao on 9/4/15.
+//  Copyright (c) 2015 Maokebing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
